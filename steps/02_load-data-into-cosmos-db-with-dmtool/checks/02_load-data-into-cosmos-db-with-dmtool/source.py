6a32321b87e8119daa6ad8ca70ac2ac518d00e15
Bablu Lawrence <v-balawrence@example.com>
2022-09-19 17:58:51 +0530

    Updated functions for validation

--- steps/02_load-data-into-cosmos-db-with-dmtool/checks/02_load-data-into-cosmos-db-with-dmtool/source.py
+++ steps/02_load-data-into-cosmos-db-with-dmtool/checks/02_load-data-into-cosmos-db-with-dmtool/source.py
@@ -1,29 +1,36 @@
-
+import logging
+from urllib import response
 from azure.identity import ClientSecretCredential
-from azure.mgmt.loganalytics import LogAnalyticsManagementClient
-from azure.mgmt.monitor import MonitorManagementClient
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+from azure.cosmos import CosmosClient
 
 def handler(event, context):
-    credentials, subscription_id = get_credentials(event)
-    resource_group = event['environment_params']['resource_group']
-    la_client = LogAnalyticsManagementClient(credentials, subscription_id)
-    mon_client = MonitorManagementClient(credentials, subscription_id)
-
-    try:
-        list_monitors = list(la_client.workspaces.list_by_resource_group(resource_group))
-        if list_monitors and any(list_monitors):
-            diag_settings = list(mon_client.diagnostic_settings.list(list_monitors[0].id))
-            if diag_settings and any(diag_settings):
-                collection_type = ""
-                for i in diag_settings[0].logs:
-                    collection_type = collection_type + i.category_group + ", "
-                
-                if "audit" in collection_type and "allLogs" in collection_type:
-                    return True
-                return False
-            return False
+    try:        
+        credentials, subscription_id = get_credentials(event)
+        url = event['environment_params']['url']
+        database_name = event['environment_params']['database_name']
+        container_name = event['environment_params']['container_name']
+        resource_group_name = event['environment_params']['resource_group_name']    
+        account_name = event['environment_params']['account_name']    
+        query = event['environment_params']['query']
+        item_count = event['environment_params']['item_count']
+        cosmos_mgmt_client = CosmosDBManagementClient(credentials, subscription_id)
+        keys = cosmos_mgmt_client.database_accounts.list_keys(resource_group_name, account_name).as_dict()
+        cosmos_client = CosmosClient(url, {'masterKey': keys['primary_readonly_master_key']})
+        db_list = cosmos_client.list_databases()
+        for db in db_list:
+            if (db['id'] == database_name):                
+                db_client = cosmos_client.get_database_client(db['id'])
+                container_list = db_client.list_containers()
+                for container in container_list:
+                    if (container['id'] == container_name):
+                        container_client = db_client.get_container_client(container_name)
+                        response = list(container_client.query_items(query, enable_cross_partition_query=True))                    
+                        if (response[0] >= item_count):
+                            return True
         return False
-    except:
+    except Exception as e:
+        logging.error(e)
         return False
 
 def get_credentials(event):
